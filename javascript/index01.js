478a1051f7b3b69e7ec1d89f13e878695beeb7ac
3h1548 <1968806729@qq.com>
2023-02-02 14:36:54 +0800

    修改

---
+++ javascript/index01.js
@@ -0,0 +1,17 @@
+// 对象数组去重
+// https://juejin.cn/post/7088216038063472670
+function arrayToHeavy(arr) {
+  const result = []
+  const mapList = new Map()
+  console.log('mapList: ', mapList);
+  arr.forEach(item => {
+    if (!mapList.has (item.id)) {
+      result.push(item)
+      mapList.set(item.id, true)
+    }
+  });
+
+  return result
+}
+
+console.log('arrayToHeavy([1,2,3,3,3,4,4]): ', arrayToHeavy([{id:1},{id:3},{id:4},{id:2,name:'111'},{id:2}]));
