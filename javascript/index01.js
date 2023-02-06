64bd01e238665324634ef97bdd5dca36eb0cc74f
3h1548 <1968806729@qq.com>
2023-02-06 10:37:58 +0800

    修改

--- javascript/index01.js
+++ javascript/index01.js
@@ -15,3 +15,13 @@ function arrayToHeavy(arr) {
 }
 
 console.log('arrayToHeavy([1,2,3,3,3,4,4]): ', arrayToHeavy([{id:1},{id:3},{id:4},{id:2,name:'111'},{id:2}]));
+
+let a = null
+console.log('a: ',typeof a);
+
+
+let var_symbol = Symbol.for('symbol');
+console.log('var_symbol: ', var_symbol);
+
+
+console.log(`[Symbol('name')]: `, [Symbol('name')] );
